6dba5c6d78494663e54d43ed8f38e78a4926554b
Omer Katz <omer.katz@omerkatz.com>
2022-01-12 13:03:15 +0200

    Allow overriding pytest arguments.

--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,7 @@ def test(session):
     requirements = session.run_always(
         "poetry", "export", "-f", "requirements.txt", "--with", "test", "--without-hashes", silent=True, external=True
     )
-    session.install(*[req for req in requirements.split("\n") if req])
+    session.install(*[req.strip() for req in requirements.split("\n") if req.strip()])
 
-    session.run("pytest", "-nauto", "--cov=pytest_celery", "--cov-branch", "--cov-report=xml")
+    test_args = session.posargs or ("-nauto", "--cov=pytest_celery", "--cov-branch", "--cov-report=xml")
+    session.run("pytest", "tests/", *test_args)
