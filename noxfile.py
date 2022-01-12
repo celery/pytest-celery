b658cc0301e245963b5a26ef6103dd3d8a13fafe
Omer Katz <omer.katz@omerkatz.com>
2022-01-12 13:14:00 +0200

    Create poetry.lock before exporting requirements.

--- noxfile.py
+++ noxfile.py
@@ -3,6 +3,7 @@ import nox
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def test(session):
+    session.run_always("poetry", "lock")
     requirements = session.run_always(
         "poetry", "export", "-f", "requirements.txt", "--with", "test", "--without-hashes", silent=True, external=True
     )
