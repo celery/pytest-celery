6cdec581f7a76feaa3bf795ea5dd0ca0c812b530
Naomi Elstein <naomi.els@omerkatz.com>
2022-01-17 20:04:17 +0200

    TDD red: Functional component tests for Message Brokers.

---
+++ tests/functional/test_message_broker_components.py
@@ -0,0 +1,26 @@
+import pytest
+import docker
+
+from pytest_celery.message_brokers.subclasses.redis_broker import RedisBroker
+
+
+@pytest.mark.parametrize("message_broker_cls", [RedisBroker])
+def test_message_broker_basic_functionality(message_broker_cls, subtests):
+    message_broker = message_broker_cls()
+
+    message_broker.start()
+    client = docker.from_env()
+
+    with subtests.test("Ensure docker container is running", message_broker_cls=message_broker_cls):
+        containers = client.containers.list()
+
+        assert len(containers) == 1
+
+    with subtests.test("Ensure message broker is responsive", message_broker_cls=message_broker_cls):
+        message_broker.client.ping()
+
+    message_broker.stop()
+
+    with subtests("Ensure container is now stopped", message_broker_cls=message_broker_cls):
+        containers = client.containers.list()
+        assert len(containers) == 0
