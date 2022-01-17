527cd23e583cbe95b9adf2ebe5b3ce59972aefdd
Naomi Elstein <naomi.els@omerkatz.com>
2022-01-17 20:12:07 +0200

    Move RedisBroker out of nesting.

--- pytest_celery/message_brokers/redis_broker.py
+++ pytest_celery/message_brokers/redis_broker.py
@@ -1,7 +1,10 @@
+from __future__ import annotations
+
 from functools import cached_property
 from typing import List
 
 from kombu import Queue
+from redis.client import Redis
 from testcontainers.redis import RedisContainer
 
 from pytest_celery.message_brokers.message_broker import MessageBroker
@@ -16,6 +19,6 @@ class RedisBroker(MessageBroker):
         super().__init__(RedisContainer())
 
     @cached_property
-    def client(self):
+    def client(self) -> Redis:
         return self.container.get_client()
 
