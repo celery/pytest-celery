ff9cf2c3707ebd02a9d3d4da56822fb60659be99
Naomi Elstein <naomi.els@omerkatz.com>
2022-01-17 15:45:16 +0200

    Add type to test fixtures.

--- tests/unit/message_brokers/test_message_broker.py
+++ tests/unit/message_brokers/test_message_broker.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 from unittest.mock import Mock, call
 
 import pytest
+from apscheduler.schedulers.background import BackgroundScheduler
 from kombu import Queue
 
+from pytest_celery.healthchecks.connection import ConnectionHealthy
+from pytest_celery.healthchecks.disk import DiskSpaceAvailable
 from pytest_celery.message_brokers.message_broker import MessageBroker
 
 
@@ -39,21 +42,21 @@ def disk_space_available() -> Mock:
     return Mock()
 
 
-def test_start(message_broker, container, healthcheck_scheduler):
+def test_start(message_broker: FakeMessageBroker, container: Mock, healthcheck_scheduler: BackgroundScheduler):
     message_broker.start()
 
     container.start.assert_called_once_with()
     healthcheck_scheduler.start.assert_called_once_with()
 
 
-def test_stop(message_broker, container, healthcheck_scheduler):
+def test_stop(message_broker: FakeMessageBroker, container: Mock, healthcheck_scheduler: BackgroundScheduler):
     message_broker.stop()
 
     container.stop.assert_called_once_with()
     healthcheck_scheduler.stop.assert_called_once_with()
 
 
-def test_context_manager(message_broker, container, healthcheck_scheduler):
+def test_context_manager(message_broker: FakeMessageBroker, container: Mock, healthcheck_scheduler: BackgroundScheduler):
     with message_broker:
         container.start.assert_called_once_with()
         healthcheck_scheduler.start.assert_called_once_with()
@@ -62,7 +65,8 @@ def test_context_manager(message_broker, container, healthcheck_scheduler):
     healthcheck_scheduler.stop.assert_called_once_with()
 
 
-def test_check_healthy(message_broker, container, healthcheck_scheduler, connection_healthy, disk_space_available):
+def test_check_healthy(message_broker: FakeMessageBroker, container: Mock, healthcheck_scheduler: BackgroundScheduler,
+                       connection_healthy: ConnectionHealthy, disk_space_available: DiskSpaceAvailable):
     message_broker.check_healthy(connection_healthy, disk_space_available)
 
     # todo use trigger and minutes constants from MessageBroker
