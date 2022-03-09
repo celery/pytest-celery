a4e9c05e514d09319a7ee2ce41b0e0c3d694de1b
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-09 17:32:37 +0200

    Fix MessageBrokerNode initialization - initialize the message broker as super's entity.

--- pytest_celery/test_services/nodes/message_broker_node.py
+++ pytest_celery/test_services/nodes/message_broker_node.py
@@ -5,7 +5,7 @@ from pytest_celery.test_services.nodes.base import Node
 class MessageBrokerNode(Node):
 
     def __init__(self, message_broker):
-        self.message_broker = message_broker
+        super().__init__(entity=message_broker)
 
     def start(self) -> str:
         """Starts up a message broker, and returns the URL of the vhost"""
