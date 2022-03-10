e34d7467b401874a7e2611234541eb97d116ccf0
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-10 14:28:07 +0200

    Remove methods from MessageBrokers which were moved up to TestService.

--- pytest_celery/test_services/message_brokers/base.py
+++ pytest_celery/test_services/message_brokers/base.py
@@ -9,8 +9,6 @@ from kombu import Queue
 from pytest_celery.healthchecks.connection import ConnectionHealthy
 from pytest_celery.healthchecks.disk import DiskSpaceAvailable
 from pytest_celery.test_services import TestService
-from pytest_celery.test_services.nodes import Node
-from pytest_celery.test_services.nodes.message_broker_node import MessageBrokerNode
 
 
 class MessageBroker(TestService, metaclass=ABCMeta):
@@ -39,16 +37,6 @@ class MessageBroker(TestService, metaclass=ABCMeta):
         """"""
         self.container.stop()
         self.healthcheck_scheduler.shutdown()
-
-    def __enter__(self) -> MessageBroker:
-        """"""
-        self.start()
-
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        """"""
-        self.stop()
 
     def check_healthy(self, connection_healthy: ConnectionHealthy, disk_space_available: DiskSpaceAvailable) -> None:
         self.healthcheck_scheduler.add_job(
