48042cfa79a18823ac29f79e4e674a436432a54f
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-10 17:54:35 +0200

    Add empty implementations to extending classes.

--- pytest_celery/test_services/nodes/message_broker_node.py
+++ pytest_celery/test_services/nodes/message_broker_node.py
@@ -1,8 +1,20 @@
+from __future__ import annotations
+
 from pytest_celery.test_services.message_brokers import MessageBroker
 from pytest_celery.test_services.nodes import Node
 
 
 class MessageBrokerNode(Node):
+
+    def create_vhost(self):
+        pass
+
+    @property
+    def should_create_vhost(self) -> bool:
+        pass
+
+    def destroy_vhost(self):
+        pass
 
     def __init__(self, message_broker: MessageBroker):
         super().__init__(message_broker)
--- tests/unit/nodes/test_base.py
+++ tests/unit/nodes/test_base.py
@@ -1,7 +1,36 @@
+from __future__ import annotations
+
+from unittest.mock import Mock
+
+import pytest
 
 from pytest_celery.test_services.nodes import Node
 
 
 class FakeNode(Node):
-    pass
+    @property
+    def should_create_vhost(self) -> bool:
+        pass
 
+    def destroy_vhost(self):
+        pass
+
+    def create_vhost(self):
+        pass
+
+
+@pytest.fixture
+def test_service() -> Mock:
+    Mock()
+
+
+@pytest.fixture
+def vhost(faker) -> str:
+    return faker.pystr()
+
+
+def test_initialization(test_service, vhost_name):
+    node = FakeNode(test_service, vhost_name)
+
+    assert node.vhost_name == vhost_name
+    assert node.test_service == test_service
