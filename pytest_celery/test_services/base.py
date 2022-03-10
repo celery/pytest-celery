31cac769136018c81ea815642b5be6125234883a
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-10 11:38:52 +0200

    Add methods to TestService

--- pytest_celery/test_services/base.py
+++ pytest_celery/test_services/base.py
@@ -1,14 +1,32 @@
 from abc import ABCMeta, abstractmethod
+from typing import ContextManager
 
 from pytest_celery.test_services.nodes.base import Node
 
 
-class TestService(metaclass=ABCMeta):
+class TestService(ContextManager, metaclass=ABCMeta):
     """The test service is responsible for instantiating a node."""
 
     def __init__(self):
         pass
 
+    def name(self):
+        """Name should be a unique ID: <session-id>-<test-service-type>-<config-hash>"""
+        pass
+
+    @abstractmethod
+    @property
+    def url(self):
+        pass
+
+    @abstractmethod
+    def start(self) -> None:
+        pass
+
+    @abstractmethod
+    def stop(self) -> None:
+        pass
+
     @abstractmethod
     def to_node(self) -> Node:
-        raise NotImplementedError
+        pass
