b26f213cf558f5fb9ca7804199ab66db34c98b5f
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-10 17:54:56 +0200

    Try to fix circular import issue with future annotations.

--- pytest_celery/test_services/message_brokers/__init__.py
+++ pytest_celery/test_services/message_brokers/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pytest_celery.test_services.message_brokers.base import MessageBroker
 from pytest_celery.test_services.message_brokers.redis_broker import RedisBroker
 
--- pytest_celery/test_services/nodes/__init__.py
+++ pytest_celery/test_services/nodes/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from pytest_celery.test_services.nodes.base import Node
 from pytest_celery.test_services.nodes.message_broker_node import MessageBrokerNode
 
