17fd8405ea189eef5a6910b57b203d817ddc423c
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-10 11:38:26 +0200

    Fix Node skeleton.

--- pytest_celery/test_services/nodes/base.py
+++ pytest_celery/test_services/nodes/base.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
+from urllib.parse import urljoin
 from abc import ABCMeta, abstractmethod
+from functools import cached_property
+
+from pytest_celery.test_services import TestService
 
 
 class Node(metaclass=ABCMeta):
@@ -6,22 +12,46 @@ class Node(metaclass=ABCMeta):
     A node is always instantiated by a MessageBroker (in the future, also by a ResultBackend).
     The node instance provides a vhost, a URL at which to access the MessageBroker.
     """
-    def __init__(self, entity):
-        self.entity = entity  # MessageBroker or ResultBackend
 
-    @abstractmethod
-    def start(self) -> str:
+    def __init__(self, test_service: TestService, vhost_name: str):
+        self.vhost_name = vhost_name
+        self.test_service = test_service  # MessageBroker or ResultBackend
+
+    def start(self):
         """Spin up a container (or reuse if already exists), returning the vhost for the entity"""
-        pass
+        self.test_service.start()
+        self.create_vhost_if_missing()
 
-    @abstractmethod
     def stop(self):
-        pass
+        self.test_service.stop()
+        self.destroy_vhost()
 
-    def __enter__(self) -> str:
+    def __enter__(self) -> Node:
         """"""
-        return self.entity.start()
+        self.start()
+
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         """"""
-        self.entity.stop()
+
+    def create_vhost_if_missing(self):
+        if self.should_create_vhost:
+            self.create_vhost()
+
+    @abstractmethod
+    def create_vhost(self):
+        pass
+
+    @abstractmethod
+    @property
+    def should_create_vhost(self) -> bool:
+        pass
+
+    @abstractmethod
+    def destroy_vhost(self):
+        pass
+
+    @cached_property
+    def url(self):
+        return urljoin(self.test_service.url, self.vhost_name)
