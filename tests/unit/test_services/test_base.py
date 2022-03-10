77f8f451c0a99564eaeb4fe9da786c5a73175807
Naomi Elstein <naomi.els@omerkatz.com>
2022-03-10 11:41:18 +0200

    Add to TestService unit test skeleton.

--- tests/unit/test_services/test_base.py
+++ tests/unit/test_services/test_base.py
@@ -4,7 +4,20 @@ from pytest_celery.test_services.nodes.base import Node
 
 
 class FakeTestService(TestService):
+    def start(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
+
+    @property
+    def url(self):
+        pass
+
     def to_node(self) -> Node:
         pass
 
+
+def test_name():
+    pass
 
