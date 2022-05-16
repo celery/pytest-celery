4729557b0e86acebbe4b1d81c25540a545a863fc
Omer Katz <omer.katz@kcg.tech>
2022-05-16 15:43:51 +0300

    Extract RedisTestServiceMixin for later use.

--- pytest_celery/test_services/message_brokers/redis.py
+++ pytest_celery/test_services/message_brokers/redis.py
@@ -10,12 +10,7 @@ from pytest_celery.test_services.message_brokers import MessageBroker
 from tests.unit.test_services.message_brokers.utils import create_url
 
 
-class RedisBroker(MessageBroker):
-    def __init__(self, test_session_id: str, port: int = None, container: RedisContainer = None):
-        container = container or RedisContainer(port_to_expose=port or 6379)
-
-        super().__init__(container, test_session_id)
-
+class RedisTestServiceMixin:
     @property
     def url(self):
         connection_pool = self.client.connection_pool
@@ -34,6 +29,13 @@ class RedisBroker(MessageBroker):
         port = connection_kwargs.pop("port", "")
 
         return create_url(scheme, username, password, host, port)
+
+
+class RedisBroker(RedisTestServiceMixin, MessageBroker):
+    def __init__(self, test_session_id: str, port: int = None, container: RedisContainer = None):
+        container = container or RedisContainer(port_to_expose=port or 6379)
+
+        super().__init__(container, test_session_id)
 
     @cached_property
     def client(self) -> Redis:
