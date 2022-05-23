7a81d471ab0356dbedfbf82aa57d8eb30e5d28de
dobosevych <dobosevych@gmail.com>
2022-05-23 19:24:55 +0300

    Added RabbitMQ broker

--- pytest_celery/test_services/mixins.py
+++ pytest_celery/test_services/mixins.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pika
+
 try:
     from functools import cached_property
 except ImportError:
@@ -54,3 +56,32 @@ class RedisTestServiceMixin:
 
     def ping(self) -> None:
         self.client.ping()
+
+
+class RabbitMQTestServiceMixin:
+    def _url(self, scheme):
+        username = self._container.RABBITMQ_DEFAULT_USER
+        password = self._container.RABBITMQ_DEFAULT_PASS
+        host = self._container.get_container_host_ip()
+        port = self._container.get_exposed_port(self._container.RABBITMQ_NODE_PORT)
+
+        uri_builder = URIBuilder().add_scheme(scheme)
+        if username or password:
+            uri_builder = uri_builder.add_credentials(username, password)
+        if host:
+            uri_builder = uri_builder.add_host(host)
+        if port:
+            uri_builder = uri_builder.add_port(port)
+
+        return uri_builder.geturl()
+
+    @cached_property
+    def get_client(self) -> pika.BlockingConnection:
+        return pika.BlockingConnection(self._container.get_connection_params())
+
+    def ping(self) -> None:
+        connection = pika.BlockingConnection(self._container.get_connection_params())
+        if connection.is_open:
+            channel = connection.channel()
+            channel.queue_declare(queue="ping")
+            channel.basic_publish(exchange="", routing_key="ping", body=b"PING")
--- tests/functional/test_test_services.py
+++ tests/functional/test_test_services.py
@@ -1,48 +1,52 @@
+import uuid
+
 import docker
 import pytest
 
-from pytest_celery.test_services.message_brokers import RedisBroker
+from pytest_celery.test_services.message_brokers import RedisBroker, RabbitMQBroker
 from pytest_celery.test_services.result_backends import RedisResultBackend
 
 
-@pytest.mark.parametrize("test_service_cls", [RedisBroker, RedisResultBackend])
+@pytest.mark.parametrize("test_service_cls", [RedisBroker, RedisResultBackend, RabbitMQBroker])
 def test_test_service_basic_functionality(test_service_cls, subtests, faker):
-    test_service = test_service_cls(faker.uuid4())
+    test_service_id = str(uuid.uuid4())
+    test_service = test_service_cls(test_service_id)
 
     test_service.start()
 
     docker_container_id = test_service._container.get_wrapped_container().id
     client = docker.from_env()
 
-    with subtests.test("Ensure docker container is running", test_service_cls=test_service_cls):
+    with subtests.test("Ensure docker container is running", test_service_cls=test_service_cls.__name__):
         containers = client.containers.list(filters={"id": docker_container_id})
         assert len(containers) == 1
 
-    with subtests.test("Ensure test service is responsive", test_service_cls=test_service_cls):
+    with subtests.test("Ensure test service is responsive", test_service_cls=test_service_cls.__name__):
         test_service.ping()
 
     test_service.stop()
 
-    with subtests.test("Ensure container is now stopped", test_service_cls=test_service_cls):
+    with subtests.test("Ensure container is now stopped", test_service_cls=test_service_cls.__name__):
         containers = client.containers.list(filters={"id": docker_container_id})
         assert len(containers) == 0
 
 
-@pytest.mark.parametrize("test_service_cls", [RedisBroker, RedisResultBackend])
+@pytest.mark.parametrize("test_service_cls", [RedisBroker, RedisResultBackend, RabbitMQBroker])
 def test_test_service_basic_functionality_context_manager(test_service_cls, subtests, faker):
-    test_service = test_service_cls(faker.uuid4())
+    test_service_id = str(uuid.uuid4())
+    test_service = test_service_cls(test_service_id)
 
     with test_service:
         docker_container_id = test_service._container.get_wrapped_container().id
         client = docker.from_env()
 
-        with subtests.test("Ensure docker container is running", test_service_cls=test_service_cls):
+        with subtests.test("Ensure docker container is running", test_service_cls=test_service_cls.__name__):
             containers = client.containers.list(filters={"id": docker_container_id})
             assert len(containers) == 1
 
-        with subtests.test("Ensure test service is responsive", test_service_cls=test_service_cls):
+        with subtests.test("Ensure test service is responsive", test_service_cls=test_service_cls.__name__):
             test_service.ping()
 
-    with subtests.test("Ensure container is now stopped", test_service_cls=test_service_cls):
+    with subtests.test("Ensure container is now stopped", test_service_cls=test_service_cls.__name__):
         containers = client.containers.list(filters={"id": docker_container_id})
         assert len(containers) == 0
