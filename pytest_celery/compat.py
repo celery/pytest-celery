f99c4a65dd24d6c56324809d30b6981e4c15cd98
dobosevych <dobosevych@gmail.com>
2022-05-24 18:48:18 +0300

    Fixed F401 error

--- pytest_celery/compat.py
+++ pytest_celery/compat.py
@@ -2,4 +2,4 @@ try:
     from functools import cached_property
 except ImportError:
     # TODO: Remove this backport once we drop Python 3.7 support
-    from cached_property import cached_property
+    from cached_property import cached_property  # noqa: F401
