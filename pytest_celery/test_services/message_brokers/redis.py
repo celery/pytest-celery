6655d7fff8c1dff8719dee394dda9d1922fa18da
dobosevych <dobosevych@gmail.com>
2022-05-25 13:31:27 +0300

    Merged

--- pytest_celery/test_services/message_brokers/redis.py
+++ pytest_celery/test_services/message_brokers/redis.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
-from functools import cached_property
+try:
+    from functools import cached_property
+except ImportError:
+    # TODO: Remove this backport once we drop Python 3.7 support
+    from cached_property import cached_property
 
 from redis.client import Redis
 from redis.connection import SSLConnection, UnixDomainSocketConnection
