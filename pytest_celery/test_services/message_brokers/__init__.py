e9dcf390a4429a94daeac9ee8158325f623f8885
dobosevych <dobosevych@users.noreply.github.com>
2022-05-30 19:48:51 +0300

    Update pytest_celery/test_services/message_brokers/__init__.py
    Co-authored-by: Omer Katz <omer.katz@omerkatz.com>

--- pytest_celery/test_services/message_brokers/__init__.py
+++ pytest_celery/test_services/message_brokers/__init__.py
@@ -4,4 +4,4 @@ from pytest_celery.test_services.message_brokers.base import MessageBroker
 from pytest_celery.test_services.message_brokers.rabbitmq import RabbitMQBroker
 from pytest_celery.test_services.message_brokers.redis import RedisBroker
 
-__all__ = ["MessageBroker", "RedisBroker", "RabbitMQBroker"]
+__all__ = ("MessageBroker", "RedisBroker", "RabbitMQBroker")
