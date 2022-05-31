0baa1c34f8c2621c098e9c880a2d5d747efeb6a4
dobosevych <dobosevych@gmail.com>
2022-05-31 14:56:35 +0300

    RabbitMQ client

--- pytest_celery/test_services/message_brokers/rabbitmq.py
+++ pytest_celery/test_services/message_brokers/rabbitmq.py
@@ -5,7 +5,7 @@ from pytest_celery.test_services.mixins import RabbitMQTestServiceMixin
 class RabbitMQBroker(RabbitMQTestServiceMixin, ResultBackend):
     @property
     def url(self):
-        return self._url("rpc")
+        return self._url("pyampq")
 
     def __repr__(self):
         return f"RabbitMQ Broker <{self.url}>"
--- tests/unit/test_services/message_brokers/test_rabbitmq.py
+++ tests/unit/test_services/message_brokers/test_rabbitmq.py
@@ -1,29 +1,36 @@
 from unittest.mock import MagicMock, patch
 
+import pytest
 from testcontainers.rabbitmq import RabbitMqContainer
 
 from pytest_celery.test_services.message_brokers import RabbitMQBroker
+from pytest_celery.test_services.result_backends import RabbitMQResultBackend
 
 
-def test_rabbitmq_broker_url(test_session_id, subtests):
+@pytest.mark.parametrize("test_service,url,representation",
+                         [(RabbitMQBroker, "pyampq://guest:guest@localhost:5672",
+                           "RabbitMQ Broker <pyampq://guest:guest@localhost:5672>"),
+                          (RabbitMQResultBackend, "rpc://guest:guest@localhost:5672",
+                           "RabbitMQ Result Backend <rpc://guest:guest@localhost:5672>")])
+def test_rabbitmq_url(test_session_id, subtests, test_service, url, representation):
     with patch.object(RabbitMqContainer, "get_exposed_port") as get_exposed_port_mocked:
         get_exposed_port_mocked.return_value = 5672
         container = RabbitMqContainer()
-        broker = RabbitMQBroker(test_session_id, container=container)
-        expected_url = "pyampq://guest:guest@localhost:5672"
+        service = test_service(test_session_id, container=container)
 
         with subtests.test("RabbitMQ Broker url"):
-            assert broker.url == expected_url
+            assert service.url == url
 
         with subtests.test("Debug representation includes original url in full"):
-            assert repr(broker) == f"RabbitMQ Broker <{expected_url}>"
+            assert repr(service) == representation
 
 
-def test_rabbitmq_broker_ping(test_session_id, subtests):
+@pytest.mark.parametrize("test_service", [RabbitMQBroker, RabbitMQResultBackend])
+def test_rabbitmq_broker_ping(test_session_id, subtests, test_service):
     with patch.object(RabbitMqContainer, "get_exposed_port") as get_exposed_port_mocked:
         get_exposed_port_mocked.return_value = 15672
         container = RabbitMqContainer()
-        broker = RabbitMQBroker(test_session_id, container=container)
+        broker = test_service(test_session_id, container=container)
         with patch("urllib.request") as urllib_request:
             f = MagicMock()
             f.read.return_value = b"""{"status": "ok"}"""
