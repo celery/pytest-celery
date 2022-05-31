a948fdb8915850c96dcc146b7948464d04c6aabb
dobosevych <dobosevych@users.noreply.github.com>
2022-05-31 15:16:51 +0300

    RabbitMQ result backend (#23)
    * RabbitMQ client
    
    * RabbitMQ client
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * RabbitMQ client
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pytest_celery/test_services/result_backends/__init__.py
+++ pytest_celery/test_services/result_backends/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pytest_celery.test_services.result_backends.base import ResultBackend
+from pytest_celery.test_services.result_backends.rabbitmq import RabbitMQResultBackend
 from pytest_celery.test_services.result_backends.redis import RedisResultBackend
 
-__all__ = ["ResultBackend", "RedisResultBackend"]
+__all__ = ("ResultBackend", "RedisResultBackend", "RabbitMQResultBackend")
