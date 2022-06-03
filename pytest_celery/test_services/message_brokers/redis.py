2b0a627db8676ed35b2a7e948c4beea990e0d08c
dobosevych <dobosevych@gmail.com>
2022-06-03 16:26:45 +0300

    Kafka broker

--- pytest_celery/test_services/message_brokers/redis.py
+++ pytest_celery/test_services/message_brokers/redis.py
@@ -2,11 +2,11 @@ from __future__ import annotations
 
 from testcontainers.redis import RedisContainer
 
+from pytest_celery.test_services.message_brokers import MessageBroker
 from pytest_celery.test_services.mixins import RedisTestServiceMixin
-from pytest_celery.test_services.result_backends import ResultBackend
 
 
-class RedisBroker(RedisTestServiceMixin, ResultBackend):
+class RedisBroker(RedisTestServiceMixin, MessageBroker):
     def __init__(self, test_session_id: str, port: int = None, container: RedisContainer = None):
         container = container or RedisContainer(port_to_expose=port or 6379)
 
--- tests/functional/test_test_services.py
+++ tests/functional/test_test_services.py
@@ -10,7 +10,7 @@ def test_session_id(faker):
     return faker.uuid4()
 
 
-@pytest.fixture(params=(RedisBroker, RedisResultBackend, RabbitMQBroker, RabbitMQResultBackend))
+@pytest.fixture(params=(RedisBroker, RedisResultBackend, RabbitMQBroker, RabbitMQResultBackend, KafkaBroker))
 def test_service(test_session_id, request):
     return request.param(test_session_id)
 
