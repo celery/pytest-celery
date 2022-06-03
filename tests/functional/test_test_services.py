391f6f66b274b7ba0023d25c283bf3576d41cfb8
dobosevych <dobosevych@gmail.com>
2022-06-03 18:49:30 +0300

    Kafka broker

--- tests/functional/test_test_services.py
+++ tests/functional/test_test_services.py
@@ -47,17 +47,17 @@ def test_test_service_basic_functionality(test_service, test_service_name, docke
         assert len(containers) == 0
 
 
-# def test_test_service_basic_functionality_context_manager(test_service, test_service_name, docker_client, subtests):
-#     with test_service:
-#         docker_container_id = get_docker_container_id(test_service)
-#
-#         with subtests.test("Ensure docker container is running", test_service_cls=test_service_name):
-#             containers = docker_client.containers.list(filters={"id": docker_container_id})
-#             assert len(containers) == 1
-#
-#         with subtests.test("Ensure test service is responsive", test_service_cls=test_service_name):
-#             test_service.ping()
-#
-#     with subtests.test("Ensure container is now stopped", test_service_cls=test_service_name):
-#         containers = docker_client.containers.list(filters={"id": docker_container_id})
-#         assert len(containers) == 0
+def test_test_service_basic_functionality_context_manager(test_service, test_service_name, docker_client, subtests):
+    with test_service:
+        docker_container_id = get_docker_container_id(test_service)
+
+        with subtests.test("Ensure docker container is running", test_service_cls=test_service_name):
+            containers = docker_client.containers.list(filters={"id": docker_container_id})
+            assert len(containers) == 1
+
+        with subtests.test("Ensure test service is responsive", test_service_cls=test_service_name):
+            test_service.ping()
+
+    with subtests.test("Ensure container is now stopped", test_service_cls=test_service_name):
+        containers = docker_client.containers.list(filters={"id": docker_container_id})
+        assert len(containers) == 0
