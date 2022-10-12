c1d5d28012401fcf8c2f8b0d41eb572cd0da2d7e
Asif Saif Uddin <auvipy@gmail.com>
2022-10-12 15:34:43 +0600

    Update pytest_celery/test_services/message_brokers/rabbitmq.py
    pyamqp

--- pytest_celery/test_services/message_brokers/rabbitmq.py
+++ pytest_celery/test_services/message_brokers/rabbitmq.py
@@ -5,7 +5,7 @@ from pytest_celery.test_services.mixins import RabbitMQTestServiceMixin
 class RabbitMQBroker(RabbitMQTestServiceMixin, MessageBroker):
     @property
     def url(self):
-        return self._url("pyampq")
+        return self._url("pyamqp")
 
     def __repr__(self):
         return f"RabbitMQ Broker <{self.url}>"
