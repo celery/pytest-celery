46218ee87b0873b73633e67283f4b00c99585606
Tomer Nosrati <d6.tomer@gmail.com>
2023-10-18 17:19:03 +0300

    Added CeleryTestNode.assert_log_exists() and moved wait_for_log() to CeleryTestNode from CeleryTestWorker

--- src/pytest_celery/api/base.py
+++ src/pytest_celery/api/base.py
@@ -5,9 +5,12 @@ from typing import Tuple
 from typing import Type
 from typing import Union
 
+import pytest_docker_tools
 from celery import Celery
+from pytest_docker_tools.wrappers.container import wait_for_callable
 
 from pytest_celery.api.container import CeleryTestContainer
+from pytest_celery.defaults import RESULT_TIMEOUT
 
 
 class CeleryTestNode:
@@ -51,6 +54,16 @@ class CeleryTestNode:
 
     def teardown(self) -> None:
         self.container.teardown()
+
+    def wait_for_log(self, log: str, message: str = "", timeout: int = RESULT_TIMEOUT) -> None:
+        message = message or f"Waiting for worker container '{self.name()}' to log -> {log}"
+        wait_for_callable(message=message, func=lambda: log in self.logs(), timeout=timeout)
+
+    def assert_log_exists(self, log: str, message: str = "", timeout: int = RESULT_TIMEOUT) -> None:
+        try:
+            self.wait_for_log(log, message, timeout)
+        except pytest_docker_tools.exceptions.TimeoutError:
+            assert False, f"Worker container '{self.name()}' did not log -> {log} within {timeout} seconds"
 
 
 class CeleryTestCluster:
