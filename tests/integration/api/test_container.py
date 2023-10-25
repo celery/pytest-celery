322aafde43d430d3b90e029fed0d7f6de212b482
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-10-25 23:01:16 +0300

    Reverted removed test by mistake: test_celery_test_container.test_client()

--- tests/integration/api/test_container.py
+++ tests/integration/api/test_container.py
@@ -7,5 +7,8 @@ from tests.defaults import ALL_COMPONENTS_FIXTURES
 
 @pytest.mark.parametrize("container", lazy_fixture(ALL_COMPONENTS_FIXTURES))
 class test_celery_test_container:
+    def test_client(self, container: CeleryTestContainer):
+        assert container.client
+
     def test_ready(self, container: CeleryTestContainer):
         assert container.ready()
