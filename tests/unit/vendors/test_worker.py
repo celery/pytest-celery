c0c63e45c9d0a430d1ff26180f752123e9a49dda
Tomer Nosrati <tomer.nosrati@kcg.tech>
2023-10-25 19:21:49 +0300

    New Tests: tests/unit/vendors/test_worker.py

--- tests/unit/vendors/test_worker.py
+++ tests/unit/vendors/test_worker.py
@@ -1,13 +1,10 @@
 import inspect
 
-import pytest
-from celery import Celery
-from pytest_lazyfixture import lazy_fixture
-
-from pytest_celery import CELERY_SETUP_WORKER
 from pytest_celery import DEFAULT_WORKER_ENV
+from pytest_celery import DEFAULT_WORKER_LOG_LEVEL
+from pytest_celery import DEFAULT_WORKER_NAME
+from pytest_celery import DEFAULT_WORKER_QUEUE
 from pytest_celery import DEFAULT_WORKER_VERSION
-from pytest_celery import CeleryTestWorker
 from pytest_celery import CeleryWorkerContainer
 
 
@@ -15,11 +12,31 @@ class test_celery_worker_container:
     def test_version(self):
         assert CeleryWorkerContainer.version() == DEFAULT_WORKER_VERSION
 
-    def test_env(self, celery_worker_cluster_config: dict):
-        assert CeleryWorkerContainer.env(celery_worker_cluster_config) == DEFAULT_WORKER_ENV
+    def test_log_level(self):
+        assert CeleryWorkerContainer.log_level() == DEFAULT_WORKER_LOG_LEVEL
+
+    def test_worker_name(self):
+        assert CeleryWorkerContainer.worker_name() == DEFAULT_WORKER_NAME
+
+    def test_worker_queue(self):
+        assert CeleryWorkerContainer.worker_queue() == DEFAULT_WORKER_QUEUE
 
     def test_tasks_modules(self):
         assert CeleryWorkerContainer.tasks_modules() == set()
+
+    def test_signals_modules(self):
+        assert CeleryWorkerContainer.signals_modules() == set()
+
+    def test_buildargs(self):
+        assert CeleryWorkerContainer.buildargs() == {
+            "CELERY_VERSION": DEFAULT_WORKER_VERSION,
+            "CELERY_LOG_LEVEL": DEFAULT_WORKER_LOG_LEVEL,
+            "CELERY_WORKER_NAME": DEFAULT_WORKER_NAME,
+            "CELERY_WORKER_QUEUE": DEFAULT_WORKER_QUEUE,
+        }
+
+    def test_env(self, celery_worker_cluster_config: dict):
+        assert CeleryWorkerContainer.env(celery_worker_cluster_config) == DEFAULT_WORKER_ENV
 
     def test_initial_content_default_tasks(self):
         from tests import tasks
@@ -41,16 +58,3 @@ class test_celery_worker_container:
 
     def test_task_modules(self):
         assert CeleryWorkerContainer.tasks_modules() == set()
-
-
-@pytest.mark.parametrize("worker", [lazy_fixture(CELERY_SETUP_WORKER)])
-class test_base_worker_api:
-    def test_ready(self, worker: CeleryTestWorker):
-        worker.ready()
-        worker.container.ready.assert_called_once()
-
-    def test_app(self, worker: CeleryTestWorker, celery_setup_app: Celery):
-        assert worker.app is celery_setup_app
-
-    def test_version(self, worker: CeleryTestWorker):
-        assert worker.version == CeleryWorkerContainer.version()
