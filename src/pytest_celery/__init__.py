84e4658d5330f93126451087e07ca4252457e60b
Tomer Nosrati <tomer.nosrati@kcg.tech>
2024-01-03 01:30:31 +0200

    Attempt to fix the version regex

--- src/pytest_celery/__init__.py
+++ src/pytest_celery/__init__.py
@@ -48,9 +48,8 @@ version_info_t = namedtuple(
 )
 
 
-# bumpversion can only search for {current_version}
-# so we have to parse the version here.
-_temp = re.match(r"(\d+)\.(\d+).(\d+)(.+)?", __version__).groups()  # type: ignore
+# Required for RTD to build
+_temp = re.match(r"(\d+)\.(\d+)\.(\d+)([a-zA-Z0-9]+)?", __version__).groups()  # type: ignore
 VERSION = version_info = version_info_t(int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or "", "")
 del _temp
 del re
