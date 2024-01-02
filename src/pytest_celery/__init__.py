49d38474a7c1216288ab334d48a6e1c5cdd053b2
Tomer Nosrati <tomer.nosrati@kcg.tech>
2024-01-02 23:11:25 +0200

    Prepare for (pre) release: v1.0.0a6 (#137)
    * Fixed error: AttributeError: 'NoneType' object has no attribute 'groups'
    
    * Bumping version from 1.0.0a5 to 1.0.0a6

--- src/pytest_celery/__init__.py
+++ src/pytest_celery/__init__.py
@@ -5,7 +5,7 @@ pytest-celery a shim pytest plugin to enable celery.contrib.pytest
 # flake8: noqa
 
 
-__version__ = "1.0.0a5"  # pragma: no cover
+__version__ = "1.0.0a6"  # pragma: no cover
 
 
 import re
@@ -49,7 +49,11 @@ version_info_t = namedtuple(
 
 # bumpversion can only search for {current_version}
 # so we have to parse the version here.
-_temp = re.match(r"(\d+)\.(\d+).(\d+)(.+)?", __version__).groups()  # type: ignore
+match = re.match(r"(\d+)\.(\d+)\.(\d+)(.+)?", __version__)
+if match:
+    _temp = match.groups()
+else:
+    raise ValueError(f"The version string '{__version__}' does not match the expected pattern.")
 VERSION = version_info = version_info_t(int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or "", "")
 del _temp
 del re
