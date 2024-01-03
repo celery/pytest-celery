b99ceaabf206c82a50d8c9173a34d68062d26411
Tomer Nosrati <tomer.nosrati@kcg.tech>
2024-01-03 02:19:55 +0200

    Version from PyPI has v prefix but does not when installed from git - this makes both supported

--- src/pytest_celery/__init__.py
+++ src/pytest_celery/__init__.py
@@ -53,7 +53,7 @@ version_info_t = namedtuple(
 
 
 # Required for RTD to build
-_temp = re.match(r"(\d+)\.(\d+)\.(\d+)([a-zA-Z0-9]+)?", __version__).groups()  # type: ignore
+_temp = re.match(r"v?(\d+)\.(\d+)\.(\d+)([a-zA-Z0-9]+)?", __version__).groups()  # type: ignore
 VERSION = version_info = version_info_t(int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or "", "")
 del _temp
 del re
