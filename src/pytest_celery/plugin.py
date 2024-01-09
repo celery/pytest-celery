819e9259b2a9cf2e203144b2c9aa8b5dbce3c664
Tomer Nosrati <tomer.nosrati@gmail.com>
2024-01-09 21:14:20 +0200

    Documented: src/pytest_celery/plugin.py

--- src/pytest_celery/plugin.py
+++ src/pytest_celery/plugin.py
@@ -1,4 +1,13 @@
-"""Pytest-celery entry point."""
+"""Pytest-celery entry point.
+
+The new pytest-celery plugin is a complete new infrastructure for
+testing Celery applications. It enables the legacy testing
+infrastructure for maintaining backwards compatibility with pytest-
+celery < 1.0.0.
+
+The legacy testing infrastructure is maintained under the celery
+project.
+"""
 
 # pytest-celery < 1.0.0 infrastructure
 from celery.contrib.pytest import *  # noqa
