f0ccea5de1a8dc7f0d793ba28c9908c4cb5b2eeb
Tomer Nosrati <tomer.nosrati@gmail.com>
2024-01-13 03:51:33 +0200

    Added myworker example

---
+++ examples/myworker/tests/myworker/myworker.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+from typing import Any
+
+import pytest
+from celery import Celery
+from pytest_docker_tools import build
+from pytest_docker_tools import container
+from pytest_docker_tools import fxtr
+
+from pytest_celery import CeleryTestWorker
+from pytest_celery import CeleryWorkerContainer
+from pytest_celery import defaults
+
+
+class MyWorkerContainer(CeleryWorkerContainer):
+    @property
+    def client(self) -> Any:
+        return self
+
+    @classmethod
+    def version(cls) -> str:
+        return "Celery main branch"
+
+    @classmethod
+    def log_level(cls) -> str:
+        return "INFO"
+
+    @classmethod
+    def worker_name(cls) -> str:
+        return "my_worker"
+
+    @classmethod
+    def worker_queue(cls) -> str:
+        return "myworker"
+
+
+myworker_image = build(
+    path=".",
+    dockerfile="tests/myworker/Dockerfile",
+    tag="pytest-celery/myworker:example",
+    buildargs=MyWorkerContainer.buildargs(),
+)
+
+
+myworker_container = container(
+    image="{myworker_image.id}",
+    environment=fxtr("default_worker_env"),
+    network="{default_pytest_celery_network.name}",
+    volumes={"{default_worker_volume.name}": defaults.DEFAULT_WORKER_VOLUME},
+    wrapper_class=MyWorkerContainer,
+    timeout=defaults.DEFAULT_WORKER_CONTAINER_TIMEOUT,
+)
+
+
+@pytest.fixture
+def myworker_worker(myworker_container: MyWorkerContainer, celery_setup_app: Celery) -> CeleryTestWorker:
+    worker = CeleryTestWorker(myworker_container, app=celery_setup_app)
+    yield worker
+    worker.teardown()
---
+++ examples/myworker/tests/test_myworker.py
@@ -0,0 +1,27 @@
+import pytest
+from celery.canvas import Signature
+from celery.result import AsyncResult
+
+from pytest_celery import RESULT_TIMEOUT
+from pytest_celery import CeleryTestSetup
+from pytest_celery import CeleryTestWorker
+from pytest_celery import CeleryWorkerCluster
+from pytest_celery import ping
+
+
+@pytest.fixture
+def celery_worker_cluster(
+    celery_worker: CeleryTestWorker,
+    myworker_worker: CeleryTestWorker,
+) -> CeleryWorkerCluster:
+    cluster = CeleryWorkerCluster(celery_worker, myworker_worker)  # type: ignore
+    yield cluster
+    cluster.teardown()
+
+
+def test_ping(celery_setup: CeleryTestSetup):
+    worker: CeleryTestWorker
+    for worker in celery_setup.worker_cluster:
+        sig: Signature = ping.s()
+        res: AsyncResult = sig.apply_async(queue=worker.worker_queue)
+        assert res.get(timeout=RESULT_TIMEOUT) == "pong"
