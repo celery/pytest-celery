bcc97b3182ffd5ed39f4d6e7e97828c281eac650
Christian Clauss <cclauss@me.com>
2024-02-03 15:22:38 +0100

    Fix failing tests by putting request first

--- tests/unit/api/test_backend.py
+++ tests/unit/api/test_backend.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import pytest
 from celery import Celery
+from pytest_lazyfixture import lazy_fixture
 
 from pytest_celery import CELERY_BACKEND
 from pytest_celery import CELERY_BACKEND_CLUSTER
@@ -10,20 +11,17 @@ from pytest_celery import CeleryBackendCluster
 from pytest_celery import CeleryTestBackend
 
 
-@pytest.mark.parametrize("backend", [CELERY_BACKEND])
+@pytest.mark.parametrize("backend", [lazy_fixture(CELERY_BACKEND)])
 class test_celey_test_backend:
-    def test_default_config_format(self, request, backend: CeleryTestBackend):
-        backend = request.getfixturevalue(backend)
+    def test_default_config_format(self, backend: CeleryTestBackend):
         assert backend.default_config()["url"] == DEFAULT_WORKER_ENV["CELERY_RESULT_BACKEND"]
         assert backend.default_config()["host_url"] == DEFAULT_WORKER_ENV["CELERY_RESULT_BACKEND"]
 
-    def test_restart_no_app(self, request, backend: CeleryTestBackend):
-        backend = request.getfixturevalue(backend)
+    def test_restart_no_app(self, backend: CeleryTestBackend):
         assert backend.app is None
         backend.restart()
 
-    def test_restart_with_app(self, request, backend: CeleryTestBackend, celery_setup_app: Celery):
-        backend = request.getfixturevalue(backend)
+    def test_restart_with_app(self, backend: CeleryTestBackend, celery_setup_app: Celery):
         backend._app = celery_setup_app
         assert "result_backend" not in celery_setup_app.conf.changes
         backend.restart()
