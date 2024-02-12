3f08a58f94ce537a1ab7e47fd8c4d25528d93a8c
Tomer Nosrati <tomer.nosrati@gmail.com>
2024-02-12 21:58:40 +0200

    Bugfix: Disabled clusters raised exception on access from setup instead of returning None (#196)

--- tests/integration/api/test_setup.py
+++ tests/integration/api/test_setup.py
@@ -4,8 +4,11 @@ import pytest
 from celery import Celery
 
 from pytest_celery import RESULT_TIMEOUT
+from pytest_celery import CeleryBackendCluster
+from pytest_celery import CeleryBrokerCluster
 from pytest_celery import CeleryTestSetup
 from pytest_celery import CeleryTestWorker
+from pytest_celery import CeleryWorkerCluster
 from tests.tasks import identity
 
 
@@ -59,3 +62,34 @@ class test_celery_test_setup_integration:
             queue = worker.worker_queue
             r = identity.s("test_ready").apply_async(queue=queue)
             assert r.get(timeout=RESULT_TIMEOUT) == "test_ready"
+
+    class test_disabling_backend_cluster:
+        @pytest.fixture
+        def celery_backend_cluster(self) -> CeleryBackendCluster:
+            return None
+
+        def test_disabling_backend_cluster(self, celery_setup: CeleryTestSetup):
+            assert celery_setup.backend_cluster is None
+            assert celery_setup.backend is None
+
+    class test_disabling_broker_cluster:
+        @pytest.fixture
+        def celery_broker_cluster(self) -> CeleryBrokerCluster:
+            return None
+
+        @pytest.fixture
+        def celery_worker_cluster(self) -> CeleryWorkerCluster:
+            return None
+
+        def test_disabling_broker_cluster(self, celery_setup: CeleryTestSetup):
+            assert celery_setup.broker_cluster is None
+            assert celery_setup.broker is None
+
+    class test_disabling_worker_cluster:
+        @pytest.fixture
+        def celery_worker_cluster(self) -> CeleryWorkerCluster:
+            return None
+
+        def test_disabling_worker_cluster(self, celery_setup: CeleryTestSetup):
+            assert celery_setup.worker_cluster is None
+            assert celery_setup.worker is None
