ae399bf0264af4c9b470f2a1177dd2d97302a17a
Tomer Nosrati <tomer.nosrati@gmail.com>
2024-03-11 15:35:38 +0200

    Hotfix: Removed after_setup_logger signal handler from the default worker app.py (#238)

--- src/pytest_celery/vendors/worker/content/app.py
+++ src/pytest_celery/vendors/worker/content/app.py
@@ -7,11 +7,8 @@ Template for Celery worker application.
 from __future__ import annotations
 
 import json
-import logging
-import sys
 
 from celery import Celery
-from celery.signals import after_setup_logger
 
 imports = None
 
@@ -21,12 +18,6 @@ config = None
 if config:
     app.config_from_object(config)
     print(f"Changed worker configuration: {json.dumps(config, indent=4)}")
-
-
-@after_setup_logger.connect
-def setup_loggers(logger: logging.Logger, *args: tuple, **kwargs: dict) -> None:
-    # https://distributedpython.com/posts/celery-docker-and-the-missing-startup-banner/
-    logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 if __name__ == "__main__":
