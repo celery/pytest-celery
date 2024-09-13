892ab7ab9fc40b0acef1789b16fad318137ce111
Tomer Nosrati <tomer.nosrati@gmail.com>
2024-09-13 15:23:36 +0300

    Removed "--maxclients 100000" from Redis containers default CMD

--- src/pytest_celery/vendors/redis/backend/fixtures.py
+++ src/pytest_celery/vendors/redis/backend/fixtures.py
@@ -64,7 +64,7 @@ def default_redis_backend_command(default_redis_backend_cls: type[RedisContainer
     Returns:
         list[str]: Docker CMD instruction.
     """
-    return default_redis_backend_cls.command("--maxclients", "100000")
+    return default_redis_backend_cls.command()
 
 
 @pytest.fixture
